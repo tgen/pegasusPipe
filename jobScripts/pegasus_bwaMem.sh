9c2aef172cb6c6c037dd9f55c59d1d78e74777fb
Ryan Richholt <rrichholt@tgen.org>
2017-09-11 10:33:12 -0700

    Debugging the job submission scripts

--- jobScripts/pegasus_bwaMem.sh
+++ jobScripts/pegasus_bwaMem.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-#PBS -S /bin/bash
 #SBATCH --job-name="pegasus_bwaMem"
 #SBATCH --time=0-72:00:00
 #SBATCH --mail-user=tgenjetstream@tgen.org
 #SBATCH --mail-type=FAIL
-#PBS -j oe
-#SBATCH --output="/${D}/oeFiles/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.out"
-#SBATCH --error="/${D}/oeFiles/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.err"
+# --output="/${D}/oeFiles/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.out"
+# --error="/${D}/oeFiles/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.err"
 
 time=`date +%d-%m-%Y-%H-%M` 
 beginTime=`date +%s`
@@ -20,6 +18,7 @@ echo "### NXT1: ${NXT1}"
 echo "### BWAPATH: ${BWAPATH}"
 
 echo "### BWA mem started for ${FASTQ1} at $time"
+echo "perf stat ${BWAPATH}/bwa mem -R ${RGTAG} -M -t8 ${REF} ${FASTQ1} ${FASTQ2} 2> ${BAMPRE}.bwaMem.perfOut | perf stat ${SAMTOOLSPATH}/samtools view -S -h -b -t ${FAI} - 2> ${BAMPRE}.samview.perfOut | perf stat ${SAMTOOLSPATH}/samtools sort - ${BAMPRE} 2> ${BAMPRE}.samsort.perfOut"
 perf stat ${BWAPATH}/bwa mem -R ${RGTAG} -M -t8 ${REF} ${FASTQ1} ${FASTQ2} 2> ${BAMPRE}.bwaMem.perfOut | perf stat ${SAMTOOLSPATH}/samtools view -S -h -b -t ${FAI} - 2> ${BAMPRE}.samview.perfOut | perf stat ${SAMTOOLSPATH}/samtools sort - ${BAMPRE} 2> ${BAMPRE}.samsort.perfOut
 if [ $? -eq 0 ] ; then
 	perf stat ${SAMTOOLSPATH}/samtools index ${BAMPRE}.bam 2> ${BAMPRE}.samindex.perfOut
