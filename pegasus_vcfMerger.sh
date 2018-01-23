ee5684ae9db261606b033d068d23217a7af84ac9
TGenJetstream <jetstream@tgen.org>
2018-01-23 10:34:49 -0700

    Added S6B recipe to vcfMerger

--- pegasus_vcfMerger.sh
+++ pegasus_vcfMerger.sh
@@ -173,6 +173,10 @@ do
         bedFile="/home/tgenref/homo_sapiens/grch37_hg19/capture_targets/illumina_nextera_v1.2/padded_targets.bed"
     elif [[ "$kitName" == *R37 ]] ; then
         bedFile="/home/tgenref/homo_sapiens/grch37_hg19/capture_targets/illumina_nextera_v1.2/NexteraRapidCaptureExomeV1.2_Targets.txt"
+    elif [[ "$kitName" == *KBM ]] ; then
+        bedFile="/home/tgenref/homo_sapiens/grch37_hg19/capture_targets/Baylor_MS_Ig_CS_SNP3/Baylor_MS_Ig_CS_SNP3_Regions.preprocessed.filter.bed"
+    elif [[ "$kitName" == *S6B ]] ; then
+        bedFile="/home/tgenref/homo_sapiens/grch37_hg19/capture_targets/agilent_human_all_exon_v6s_xt_beta/Human_All_Exon_v6S_XT_beta_targets.preprocessed.filter.bed"
     fi
 
     echo "### BED FILE= $bedFile"
