998f2f4b11ed4565c85602285178b7bc0b25898d
TGenJetstream <jetstream@tgen.org>
2018-11-27 09:11:45 -0700

    Wrapped variable in quotes for sbatch command

--- pegasus_dnaAlignParts.sh
+++ pegasus_dnaAlignParts.sh
@@ -135,7 +135,7 @@ do
                                     rgTag="@RG\tID:${RG_ID}\tSM:$samName\tPL:${RG_PL}\tCN:${RG_CN}\tPU:${RG_PU}\tLB:${RG_LB}\tKS:${INDEX}"
                                 fi
                                 echo "$rgTag"
-                                sbatch --account ${debit} --output $runDir/oeFiles/%x-slurm-%j.out -n 1 -N 1 --cpus-per-task $nCores --mem-per-cpu 5000 --export ALL,D=$d,RGTAG=$rgTag,FASTQ1=$thisMiniFq,FASTQ2=$thisMiniR2,REF=$ref,BWAPATH=$bwaPath,SAMTOOLSPATH=$samtoolsPath,FAI=$faiFile,BAMPRE=$bamMiniPre,RUNDIR=$runDir,NXT1=$nxtStep1,NXT2=$nxtStep2,D=$d ${JETSTREAM_HOME}/pegasusPipe/jobScripts/pegasus_bwaMem.sh
+                                sbatch --account ${debit} --output $runDir/oeFiles/%x-slurm-%j.out -n 1 -N 1 --cpus-per-task $nCores --mem-per-cpu 5000 --export ALL,D=$d,RGTAG="$rgTag",FASTQ1=$thisMiniFq,FASTQ2=$thisMiniR2,REF=$ref,BWAPATH=$bwaPath,SAMTOOLSPATH=$samtoolsPath,FAI=$faiFile,BAMPRE=$bamMiniPre,RUNDIR=$runDir,NXT1=$nxtStep1,NXT2=$nxtStep2,D=$d ${JETSTREAM_HOME}/pegasusPipe/jobScripts/pegasus_bwaMem.sh
                                 if [ $? -eq 0 ] ; then
                                     touch $bamMiniName.dnaAlignInQueue
                                 else
